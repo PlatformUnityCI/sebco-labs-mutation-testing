e70999561f83bb006fa462920942a7b76fc980a2
SebasCouto <sebastian.couto@uala.com.ar>
2024-02-28 09:44:13 -0300

    Set up PYTHONPATH

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='tech_red_qa',
     version='0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(),  # No es necesario el argumento 'where' porque la estructura coincide ahora
     install_requires=[
         # tus dependencias aquí
     ],
