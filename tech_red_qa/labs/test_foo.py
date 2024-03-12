bd522438c267d0a2490943cfa0072407ede059a7
Sebitas <couto.cvas@gmail.com>
2024-03-12 08:48:44 -0300

    tech-red-qa-artifacts-fix-test-foo-module-reference

---
+++ tech_red_qa/labs/test_foo.py
@@ -0,0 +1,8 @@
+from lib_core.labs.my_code import foo
+import logging
+
+def test_foo():
+    result = foo()
+    print("Resultado de foo:", result)
+    assert result == "¡Hola, mundo!", "la prueba no salió exitosa"
+    logging.info("la prueba salió exitosa") 
